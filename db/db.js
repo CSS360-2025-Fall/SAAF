6424106a353c840ab1852300a29da2fdcbc4d9da
Simon Upton <66577371+simon-upton@users.noreply.github.com>
2025-10-17 14:20:03 -0700

    Fix firestore api key import

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-import { serviceAccount } from "../saaf-db-firebase-adminsdk-key.json";
+import { serviceAccount } from "../saaf-db-firebase-adminsdk-key.json" with {type: "json"};
 
 initializeApp({
   credential: cert(serviceAccount),
